9873cfeedd86b79ca1bbb5216e2263973ea9dba0
dipesh <jaswanidipesh8@gmail.com>
2023-06-16 05:14:17 +0530

    shooting sites done

--- app/gallery/[images]/page.jsx
+++ app/gallery/[images]/page.jsx
@@ -1,12 +1,68 @@
 "use client"
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useRouter } from 'next/navigation'
+import Masonry from 'react-masonry-css';
+import Image from 'next/image';
+import { Brick, Dhobi, Ganga, Ganges, Nagas, Sadhu, Mud, Streets } from '../../../components/Home/ImageData'
 
-const Images = ({params}) => {
-   
-   console.log(params)
+
+
+
+
+const Images = ({ params }) => {
+
+  const breakpointColumnsObj = {
+    default: 3,
+    1100: 2,
+    700: 1,
+  };
+
+  const [Images, setImages] = useState([]);
+
+  const handleImages = () => {
+    if (params.images == 'brick') {
+      setImages(Brick)
+    }
+    else if (params.images == 'dhobi') {
+      setImages(Dhobi)
+    }
+    else if (params.images == 'ganga') {
+      setImages(Ganga)
+    }
+    else if (params.images == 'ganges') {
+      setImages(Ganges)
+    }
+    else if (params.images == 'nagas') {
+      setImages(Nagas)
+    }
+    else if (params.images == 'sadhu') {
+      setImages(Sadhu)
+    }
+    else if (params.images == 'mud') {
+      setImages(Mud)
+    }
+    else if (params.images == 'streets') {
+      setImages(Streets)
+    }
+  }
+
+  useEffect(() => {
+    handleImages(params)
+  }, [])
+
+
   return (
-    <div>{params.images}</div>
+    <div className="card-list">
+      {Images.map((pic) => <div className="card">
+        <Image
+          className="card-image"
+          alt={`${pic.id}`}
+          src={pic.src}
+          width={500}
+          height={500}
+        ></Image>
+      </div>)}
+    </div>
   )
 }
 
--- components/Home/ShootingSites.jsx
+++ components/Home/ShootingSites.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import GlassmorphicImage from './ImageComponent'
 import pic from './assets/1.jpg'
+import {Brick,Dhobi,Ganga,Ganges,Nagas,Sadhu,Mud,Streets} from './ImageData'
+
 
 const ShootingSites = () => {
     return (
         <div>
             <h1 class="text-center text-3xl sm:text-3xl md:text-3xl lg:text-4xl xl:text-4xl font-sang ">Shooting Sites</h1>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-                <GlassmorphicImage path={"dhobi"} src={pic} heading="Ganga"/>
-                <GlassmorphicImage path={"dhobi"} src={pic} heading="Ganga"/>
-                <GlassmorphicImage path={"dhobi"} src={pic} heading="Ganga"/>
-                <GlassmorphicImage path={"dhobi"} src={pic} heading="Ganga"/>
+            <div className="flex flex-wrap justify-center">
+                <GlassmorphicImage path={"brick"} src={Brick[0].src} heading="Brick Factory"/>
+                <GlassmorphicImage path={"dhobi"} src={Dhobi[3].src} heading="Dhobi Ghat"/>
+                <GlassmorphicImage path={"ganga"} src={Ganga[3].src} heading="Ganga Aarti"/>
+                <GlassmorphicImage path={"ganges"} src={Ganges[1].src} heading="Ganges Shore Line"/>
+                <GlassmorphicImage path={"nagas"} src={Nagas[0].src} heading="Nagas and Aghoris"/>
+                <GlassmorphicImage path={"sadhu"} src={Sadhu[1].src} heading="Sadhu Boat Shoot"/>
+                <GlassmorphicImage path={"mud"} src={Mud[0].src} heading="Mude Wrestling"/>
+                <GlassmorphicImage path={"streets"} src={Streets[0].src} heading="Streets and Lanes"/>
             </div>
         </div>
     )
