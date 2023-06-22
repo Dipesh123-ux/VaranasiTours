746e7167317bb1f5812a5504edf1bf5b362d7698
dipesh <jaswanidipesh8@gmail.com>
2023-06-23 03:17:58 +0530

    css-fixed

--- components/Home/ToursAndWorkShops.jsx
+++ components/Home/ToursAndWorkShops.jsx
@@ -6,7 +6,7 @@ const ToursAndWorkShops = () => {
     return (
         <div>
             <h1 class="text-center text-3xl sm:text-3xl md:text-3xl lg:text-4xl xl:text-4xl font-sang ">Tours and Workshops</h1>
-            <div className="grid grid-cols-1 md:grid-cols-2 mx-10 md:mx-20">
+            <div className="grid grid-cols-1 md:grid-cols-2  md:mx-20">
                 <GlassmorphicImage src={pic}  heading="PHOTO TOURS" text="If you are planning to visit" path="photo-tours"/>
                 <GlassmorphicImage src={pic} heading={"WORKSHOPS"} text={"After doing two years of constant"} path="workshops"/>
                 <GlassmorphicImage src={pic} heading={"PHOTOWALKS"} text={"Visiting a new location for photo"} path="photowalks"/>
