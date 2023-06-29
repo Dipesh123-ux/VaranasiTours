0d14dc11ba702a3dff45ab74772dc7edf81e7567
Priyanshu Rajput <priyanshurajput822400@gmail.com>
2023-06-29 22:40:36 +0530

    review page redesigned

--- components/Reviews/Video.jsx
+++ components/Reviews/Video.jsx
@@ -4,13 +4,14 @@ function Video() {
     return (
 
         <div className=" h-screen">
-            <iframe 
-                className="w-full h-full"
-                src="https://video.wixstatic.com/video/7dae14_8600e275e4b44bcf80670393cd0634b7/720p/mp4/file.mp4"
+            <iframe
+                class="w-full h-full"
+                src="https://www.youtube.com/embed/lCX9fgo5y-I"
                 title="YouTube Video"
                 allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
+                allowfullscreen
             ></iframe>
+
             {/* <video controls autoPlay className='h-auto w-screen ' >
                 <source src={'https://video.wixstatic.com/video/7dae14_8600e275e4b44bcf80670393cd0634b7/720p/mp4/file.mp4'} type="video/mp4" />
             </video> */}
