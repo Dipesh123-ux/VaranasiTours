23d62df3b272b48059c51eeee1dffb812e5a7165
Priyanshu Rajput <priyanshurajput822400@gmail.com>
2023-07-04 19:44:49 +0530

    size fixed of speciality heading

--- components/Home/Speciality.jsx
+++ components/Home/Speciality.jsx
@@ -9,14 +9,14 @@ const Speciality = () => {
                     <div className='text-center text-2xl'>
                         <i className="fa fa-camera"></i>
                     </div>
-                    <h1 className='text-center text-lg lg:text-2xl mb-2 md:mb-4 '>For Photographers</h1>
+                    <h1 className='text-center text-xl lg:text-2xl mb-2 md:mb-4 '>For Photographers</h1>
                     <p>From the outset, we have always understood that a photographer's needs can only be truly comprehended by another photographer. Our extensive experience and profound understanding of the obstacles encountered in travel photography in India allow us to transform this understanding into a reality. Our goal is to provide unwavering support, ensuring that you can completely focus on capturing the perfect shots.</p>
                 </div>
                 <div className='bg-white rounded shadow-lg p-2 md:p-4 mb-8'>
                     <div className='text-center text-2xl'>
                     <i class="fa fa-regular fa-image"></i>
                     </div>
-                    <h1 className='text-center text-lg lg:text-2xl mb-2 md:mb-4 '>By Photographers</h1>
+                    <h1 className='text-center text-xl lg:text-2xl mb-2 md:mb-4 '>By Photographers</h1>
                     <p>As a collective of artists, we are committed to delivering exceptional services to our fellow foreign photographers. We transcend the conventional role of guides and embrace the identity of modern hospitality ambassadors. Drawing from our own experiences as photographers, we possess an innate understanding of your needs, allowing us to effortlessly meet and exceed your expectations. Our dedication to providing a service that is unparalleled sets us apart from the rest.</p>
                 </div>
             </div>
