0746d6dc90135526c22f634eeae073ee041f4535
Priyanshu Rajput <priyanshurajput822400@gmail.com>
2023-07-14 00:00:18 +0530

    footer responsive done

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -41,41 +41,21 @@ const Footer = () => {
 
   return (
 
-    <footer class="footer_wrapper mt-8  py-8 text-black border-t-2 drop-shadow-md">
+    <footer class="footer_wrapper mt-8  py-8 border-t-2 text-black flex justify-center drop-shadow-md">
       <div class="container ">
         <section class="page_section contact" id="contact">
           <div class="contact_section">
             <h2 class="text-2xl md:text-3xl  text-center mt-4 mb-12 font-sang font-medium">Contact Us for Bookings</h2>
           </div>
-          <div className='mb-8'>
-            {/* <div class="">
-              <div class="text-xl font-ste">
-                <h4 class="font-bold">UNIQUE Infoway</h4>
-                <p>104, Some street, NewYork, USA</p>
-              </div>
-              <div class="text-xl font-ste mt-5">
-                <h4 class="font-bold">Call us</h4>
-                <p>+1 234 567890</p>
-              </div>
-              <div class="text-xl font-ste mt-5">
-                <h4 class="font-bold">Email us</h4>
-                <p>support@sitename.com</p>
-              </div>
-
-              <ul class="flex justify-start mt-5">
-                <li><a href="javascript:void(0)" class="text-3xl mx-2 hover:text-[#00acee]"><i class="fab fa-twitter"></i></a></li>
-                <li><a href="javascript:void(0)" class="text-3xl mx-2 hover:text-[#3b5998]"><i class="fab fa-facebook"></i></a></li>
-                <li><a href="javascript:void(0)" class="text-3xl mx-2 hover:text-[#c8232c]"><i class="fab fa-pinterest"></i></a></li>
-                <li><a href="javascript:void(0)" class="text-3xl mx-2 hover:text-[#c71610]"><i class="fab fa-google-plus"></i></a></li>
-              </ul>
-            </div> */}
+          <div className='mb-8 '>
+           
 
             <form ref={form} onSubmit={sendEmail} class="form text-black  ">
-              <div className='flex justify-center  gap-8 '>
+              <div className='flex md:flex-row flex-col justify-center justify-items-center gap-2 mx-4 lg:gap-8  '>
                 <div>
-                  <input class="input-text mb-4 p-2 w-full border  bg-zinc-50  border-gray-300 rounded" type="text" name="name" placeholder='Name' />
-                  <input class="input-text mb-4 p-2 w-full border bg-zinc-50 border-gray-300 rounded" type="text" name="email" placeholder='Email' />
-                  <input class="input-text mb-4 p-2 w-full border bg-zinc-50 border-gray-300 rounded" type="text" placeholder='Subject' />
+                  <input class="input-text mb-4 p-2 w-full md:w-5/6 lg:w-full border  bg-zinc-50  border-gray-300 rounded" type="text" name="name" placeholder='Name' />
+                  <input class="input-text mb-4 p-2 w-full md:w-5/6 lg:w-full border bg-zinc-50 border-gray-300 rounded" type="text" name="email" placeholder='Email' />
+                  <input class="input-text mb-4 p-2 w-full md:w-5/6 lg:w-full border bg-zinc-50 border-gray-300 rounded" type="text" placeholder='Subject' />
                 </div>
                 <div className='text-center'>
                   <textarea class="input-text text-area mb-2 p-2 w-full border bg-zinc-50 border-gray-300 rounded h-24 " cols="0" rows="0" name="message" placeholder='Message'></textarea>
