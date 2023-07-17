488a1363037b023e9f13c284861147bdd24d1bde
Priyanshu Rajput <priyanshurajput822400@gmail.com>
2023-07-17 22:36:35 +0530

    profile pictures added

--- components/Reviews/TestimonialDetail1.jsx
+++ components/Reviews/TestimonialDetail1.jsx
@@ -1,14 +1,14 @@
 const TestimonialDetails1 = [
   {
     id: 0,
-    name: 'Michael Sorkin',
+    name: 'Micheal Sorkin',
     username: '@jakethomasphotography',
     place:'Germany',
     nickname:'Michael',
     text: 'Whenever I’m in India, I never hesitate to travel with my go-to companion, Shivam. Over time,we have explored almost every nook and cranny...',
     link: '/reviews/peoples',
     feeling: 'Whenever I’m in India, I never hesitate to travel with my go-to companion, Shivam. Over time,we have explored almost every nook and cranny of this incredible country together. He hasbecome my trusted guide, offering unique and unforgettable experiences tailored to my interests.From thrilling road biking trips to immersing ourselves in the vibrant atmosphere of Indian fairs,Shivam has always been my go-to person. His knowledge and passion for India are unparalleled, and he knows exactly how to curate the perfect adventure.Through our shared experiences, a strong bond has formed between us. Shivam has not only become my reliable travel partner but also a close friend. His dedication to ensuring my satisfaction and his commitment to creating memorable moments sets him apart. Worth hiring for their services in India..',
-    profilePic: 'no_profile',
+    profilePic: 'micheal-sorkin',
     pics: [],
     gender:"he",
 
@@ -36,7 +36,7 @@ const TestimonialDetails1 = [
     text: 'India is an absolute treasure trove for those passionate about capturing the authenticity of real  life and real people through the lens...',
     link: '/reviews/peoples',
     feeling: 'India is an absolute treasure trove for those passionate about capturing the authenticity of real  life and real people through the lens of a camera. My time spent walking with Shubham was truly remarkable, as his courteous approach and expertise allowed me to immerse myself in the vibrant pulse of India. For an exceptional photography journey that delves directly into the core of this lively country, I wholeheartedly recommend hiring Shubham and experiencing the magic firsthand.',
-    profilePic: 'no_profile',
+    profilePic: 'rosela',
     pics: [],
     gender: "she",
 
