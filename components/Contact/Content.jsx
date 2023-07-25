ba3360cc1dbbd9a32053d3d5ba121afa2c448240
Priyanshu Rajput <priyanshurajput822400@gmail.com>
2023-07-25 23:35:20 +0530

    poster updated

--- components/Contact/Content.jsx
+++ components/Contact/Content.jsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 const Content = () => {
   return (
     <div >
-      <Image alt='photo' src={poster}></Image>
+      <Image className=' lg:h-auto object-fit' alt='photo' src={poster}></Image>
       <div className='mt-8 drop-shadow-xl text-center flex justify-center'>
         <p className='w-11/12 md:w-1/2 text-xl font-ste font-medium text-center'>Get in touch with us for any questions. We are always happy to have you and work with you.</p>
       </div>
